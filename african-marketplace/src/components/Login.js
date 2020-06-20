369b780112f4aa4bb3d35346b67dd111bcdd0b7c
Tatiana Zhizhimontova <tatyana.zhi@gmail.com>
2020-06-20 13:40:29 -0700

    user account is added

--- african-marketplace/src/components/Login.js
+++ african-marketplace/src/components/Login.js
@@ -13,7 +13,7 @@ import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles, withStyles } from "@material-ui/core/styles";
 import { useSelector, useDispatch } from "react-redux";
-import { toggleMenuBar } from "../actions/index";
+import { authMenuBar } from "../actions/index";
 
 const Login = (props) => {
   const classes = useStyles();
@@ -39,7 +39,7 @@ const Login = (props) => {
         localStorage.setItem("token", res.data.token);
         localStorage.setItem("userID", res.data.user_id);
         props.history.push("/dashboard");
-        dispatch(toggleMenuBar(true));
+        dispatch(authMenuBar(true));
       })
       .catch((err) => {
         console.log("error returned from login post request", err);
